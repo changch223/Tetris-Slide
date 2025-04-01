19ac57b24d9d013caab765bd3a9148970840da92
Chia-Wei <chang.ch23@gmail.com>
2025-04-01 01:10:09 -0400

    update ui

---
+++ NumberClashTests/Reverse2048Tests.swift
@@ -0,0 +1,16 @@
+//
+//  NumberClashTests.swift
+//  NumberClashTests
+//
+//  Created by chang chiawei on 2025-03-28.
+//
+
+import Testing
+
+struct NumberClashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
